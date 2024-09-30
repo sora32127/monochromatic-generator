03ae5824c4614c416ddda6416ceb9d363ba6accb
contradiction29 <sora32127@gmail.com>
2024-09-30 22:00:26 +0900

    Introduce Hono RPC

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -1,9 +1,20 @@
 import { Hono } from 'hono'
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const schema = z.object({
+  name: z.string(),
+  age: z.number()
 })
 
-export default app
+const routes = app.post('/api/users', zValidator('json', schema), (c) => {
+  const data = c.req.valid('json')
+  return c.json({
+    message: `${data.name} is ${data.age} years old `
+  })
+})
+
+export default routes
+export type AppType = typeof routes
